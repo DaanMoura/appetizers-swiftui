97f7e341d7303d6f2c349e79385edc91065832d4
Daniel Moura <daniel.lucio.moura@gmail.com>
2024-02-06 23:52:30 -0300

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Daniel Moura on 06/02/24.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
